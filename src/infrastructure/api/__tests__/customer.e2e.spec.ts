481b7c5b63c9fb7190a7b83c1ef5d409a02599a6
wallisonmoura <wallison_cedaspy@hotmail.com>
2022-12-20 15:34:52 -0300

    creation of the api layer, customer e2e build and test implementation

--- src/infrastructure/api/__tests__/customer.e2e.spec.ts
+++ src/infrastructure/api/__tests__/customer.e2e.spec.ts
@@ -40,4 +40,45 @@ describe('E2E test for customer', () => {
     expect(response.status).toBe(500)
   })
 
+  it('Should list all customer', async () => {
+    const response = await request(app)
+    .post('/customer')
+    .send({
+      name: 'teste1',
+      address: {
+        street: 'street',
+        number: 1,
+        zip: '123456',
+        city: 'city'
+      }
+    })
+    expect(response.status).toBe(200)
+
+    const response1 = await request(app)
+    .post('/customer')
+    .send({
+      name: 'teste2',
+      address: {
+        street: 'street 2',
+        number: 12,
+        zip: '1234567',
+        city: 'city 2'
+      }
+    })
+    expect(response1.status).toBe(200)
+
+    const listResult = await request(app).get('/customer').send()
+    expect(listResult.status).toBe(200)
+    expect(listResult.body.customers.length).toBe(2)
+
+    const customer = listResult.body.customers[0]
+    expect(customer.name).toBe('teste1')
+    expect(customer.address.street).toBe('street')
+
+    const customer1 = listResult.body.customers[1]
+    expect(customer1.name).toBe('teste2')
+    expect(customer1.address.street).toBe('street 2')
+
+  })
+
 })
